5c5b13264d2a4668ce5943f9280c1be81f076009
Ethan <ethansteele@hotmail.co.uk>
2023-02-23 16:17:31 +0000

    part 2

--- server.js
+++ server.js
@@ -1,19 +1,42 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// Importing Models
 const {Restaurant} = require("./models/index")
 const {sequelize} = require("./db");
 
 const port = 3000;
 
-//TODO: Create your GET Request Route Below: 
+// Get Request
 app.get('/restaurants/:id', async (req, res) => {
-    let id = req.params.id;
-    const restaurant = await Restaurant.findByPk(id);
-
+    const restaurant = await Restaurant.findByPk(req.params.id);
     res.json(restaurant);
 });
 
+// Create Request
+app.post('/restaurants/:id', async (req, res) => {
+    const restaurant = await Restaurant.create(req.body);
+    res.json(restaurant);
+});
 
+// Update Request
+app.put('/restaurants/:id', async (req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.id);
+    await restaurant.update(req.body);
+    res.json(restaurant);
+});
+
+// Delete Request
+app.delete('/restaurants/:id', async (req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.id);
+    await restaurant.destroy();
+});
+
+// Loading Server
 app.listen(port, () => {
     sequelize.sync();
     console.log("Your server is listening on port " + port);
