c07653307be18b3123bf581173adcaf1311007df
Ethan <ethansteele@hotmail.co.uk>
2023-02-24 23:18:26 +0000

    small change

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -7,13 +7,13 @@ const {Restaurant} = require("../models/index")
 const {sequelize} = require("../db");
 
 // Get Request
-router.get('/restaurants/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     const restaurant = await Restaurant.findByPk(req.params.id);
     res.json(restaurant);
 });
 
 // Create Request
-router.post('/restaurants/:id', check(["name", "location", "cuisine"]).not().isEmpty().trim(), async (req, res) => {
+router.post('/:id', check(["name", "location", "cuisine"]).not().isEmpty().trim(), async (req, res) => {
     const errors = validationResult(req);
 
     if(!errors.isEmpty()) {
@@ -25,14 +25,14 @@ router.post('/restaurants/:id', check(["name", "location", "cuisine"]).not().isE
 });
 
 // Update Request
-router.put('/restaurants/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
     const restaurant = await Restaurant.findByPk(req.params.id);
     await restaurant.update(req.body);
     res.json(restaurant);
 });
 
 // Delete Request
-router.delete('/restaurants/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
     const restaurant = await Restaurant.findByPk(req.params.id);
     await restaurant.destroy();
 });
